810143f22e7f74e54ff785967935cf4538fc01f9
Ju Wang <jwang@vsu.edu>
2024-04-23 21:54:38 -0400

    update

--- run_script.bat
+++ run_script.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Users\Ju Wang\Desktop\scheduled_task\"
+cd "C:\Users\qingy\Desktop\scheduled_task\"
 echo "%CD%"
 python  "C:\Users\Ju Wang\Desktop\scheduled_task\hello_world.py"
 pause

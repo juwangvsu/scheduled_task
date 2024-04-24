a14bc9f9d026f4a82f1ff06634831193d1e820b3
ju wang <jwang@vsu.edu>
2024-04-23 20:43:19 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scheduled_task
+edit run_script.bat to setup path for both python and hello_world.py
---
+++ run_script.bat
@@ -0,0 +1,4 @@
+@echo off
+cd “C:\Users\Ju Wang\Desktop\scheduled_task\”
+python hello_world.py
+pause
